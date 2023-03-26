1f24d754788d6652cfe143c4be1dfd89794b58db
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2023-03-26 11:44:26 +0200

    Loader fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import { CircularProgress, Stack } from "@mui/material";
 
 const Loader = () => (
-  <Stack
-    // direction="row"
-    // justifyContent="flex-start"
-    // alignItems="center"
-    // height="80vh"
-    marginX="43vw"
-    marginY="40vh"
-  >
+  <Stack marginX="43vw" marginY="40vh">
     <CircularProgress />
   </Stack>
 );
